7711c3fd13f7dce3b4c554cd1b7367c1c0acd47b
antonchepelev <anton.chepelev@gmail.com>
2024-09-17 18:03:42 -0400

    completed task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double SALES_TAX = .05;
+        double myItemPrice = 7.80;
+        double myItemSalesTax = myItemPrice * SALES_TAX;
+        System.out.println("my item is $"+myItemPrice);
+        System.out.println("my item's sales tax is: "+ myItemSalesTax);
     }
 }
